373231460318f7e4e138acc33e49710dbdbc5e77
Harry-Gupenec <Spektar001@mail.ru>
2024-03-23 20:55:11 +0300

    refactor: delete folder session-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AppProvider } from "@/components/session-provider/AppContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AppProvider>{children}</AppProvider>
+        {children}
       </body>
     </html>
   );
