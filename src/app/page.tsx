942dc5d50698ee88ac19fc01267bea6c93227d03
Harry-Gupenec <Spektar001@mail.ru>
2024-03-23 21:11:28 +0300

    feat: add buttons login ang register for the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Homepage</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex gap-4">
+        <Link
+          className="w-[150px] text-center text-lg font-medium p-7 bg-teal-500 hover:bg-[#00726e] text-[--text] rounded-md transition-settings"
+          href={"/login"}
+        >
+          Login
+        </Link>
+        <Link
+          className="w-[150px] text-center text-lg font-medium p-7 bg-teal-500 hover:bg-[#00726e] text-[--text] rounded-md transition-settings"
+          href={"/registration"}
+        >
+          Registration
+        </Link>
+      </div>
+    </div>
+  );
 }
